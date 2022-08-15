700dfcaa9327fc4d9166d9813ee23440c67a2ecd
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-08-15 20:15:34 +0530

    Time: 3 ms (95.48%), Space: 45.9 MB (25.18%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    List<List<Integer>> res;
+
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        res = new ArrayList<>();
+        helper(nums, target, 0, new ArrayList<>());
+        return res;
+    }
+
+    void helper(int[] nums, int target, int ind, List<Integer> temp) {
+        if (target == 0) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        if (ind == nums.length) return;
+        if (nums[ind] <= target) {
+            temp.add(nums[ind]);
+            helper(nums, target - nums[ind], ind, temp);
+            temp.remove(temp.size() - 1);
+        }
+        helper(nums, target, ind + 1, temp);
+    }
+}
