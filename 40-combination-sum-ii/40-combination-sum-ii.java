f2e8721b03d98bdbd800dbf4496b5bbc4b6e094e
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-08-15 20:35:30 +0530

    Time: 4 ms (89.57%), Space: 44.1 MB (36.82%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.java
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -1,5 +1,6 @@
 class Solution {
-List<List<Integer>> res;
+    List<List<Integer>> res;
+
     public List<List<Integer>> combinationSum2(int[] nums, int target) {
         Arrays.sort(nums);
         res = new ArrayList<>();
@@ -8,14 +9,17 @@ List<List<Integer>> res;
     }
 
     void helper(int[] nums, int target, int ind, List<Integer> temp) {
+        if (target == 0) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
         if (ind == nums.length) {
-            if (target == 0) res.add(new ArrayList<>(temp));
             return;
         }
 
         if (nums[ind] <= target) {
             temp.add(nums[ind]);
-            helper(nums, target - nums[ind],ind+1, temp);
+            helper(nums, target - nums[ind], ind + 1, temp);
             temp.remove(temp.size() - 1);
         }
         int i;
