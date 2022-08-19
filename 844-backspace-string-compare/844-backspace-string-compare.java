86d4058b228b23580692de94b4288faed7cd3d9b
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-08-19 19:54:29 +0530

    Time: 1 ms (94.73%), Space: 42 MB (66.81%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,23 +1,21 @@
 class Solution {
 
     public boolean backspaceCompare(String s, String t) {
-        Stack<Character> stk1 = new Stack<>();
-        Stack<Character> stk2 = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch == '#') {
-                if (!stk1.isEmpty()) stk1.pop();
-            } else stk1.push(ch);
-        }
-        for (int i = 0; i < t.length(); i++) {
-            char ch = t.charAt(i);
-            if (ch == '#') {
-                if (!stk2.isEmpty()) stk2.pop();
-            } else stk2.push(ch);
-        }
-        if (stk1.size() != stk2.size()) return false;
-        while (!stk1.isEmpty()) {
-            if (stk1.pop() != stk2.pop()) return false;
+        int i = s.length() - 1, j = t.length() - 1, skipS = 0 , skipT = 0;
+        while(i>=0 || j>=0){
+            while(i >= 0){
+                if(s.charAt(i) == '#') {skipS++; i--;}
+                else if(skipS > 0) {skipS--; i--;}
+                else break;
+            }
+            while(j >= 0){
+                if(t.charAt(j) == '#') {skipT++; j--;}
+                else if(skipT > 0){skipT-- ;  j--;}
+                else break;
+            }
+            if(i>=0 && j>=0 && s.charAt(i) != t.charAt(j)) return false;
+            if(i>=0 != j>=0) return false;
+            i--; j--;
         }
         return true;
     }
