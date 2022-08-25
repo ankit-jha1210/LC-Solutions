12350aa118ec0493cbb5fe7341eeef97c13e110e
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-08-25 18:50:12 +0530

    Time: 11 ms (93.27%), Space: 50.9 MB (29.27%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,28 +1,27 @@
-class Pair {
-    int first;
-    int second;
-
-    Pair(int first, int second) {
-        this.first = first;
-        this.second = second;
-    }
-}
-
 class Solution {
 
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.second - b.second);
         for (int num : nums) map.put(num, map.getOrDefault(num, 0) + 1);
-        map.forEach(
-            (key, value) -> {
-                pq.add(new Pair(key, value));
-                if (pq.size() > k) pq.poll();
+        List<Integer> buckets[] = new ArrayList[nums.length + 1];
+        //         creating buckets
+        for (int key : map.keySet()) {
+            int freq = map.get(key);
+            if (buckets[freq] == null) {
+                buckets[freq] = new ArrayList<>();
             }
-        );
-        int[] arr = new int[k];
-        int i = 0;
-        while (!pq.isEmpty()) arr[i++] = pq.poll().first;
-        return arr;
+            buckets[freq].add(key);
+        }
+        int[] res = new int[k];
+        int idx = 0;
+        for (int i = buckets.length - 1; i >= 0; i--) {
+            if (buckets[i] != null) {
+                for (int val : buckets[i]) {
+                    res[idx++] = val;
+                    if (idx == k) return res;
+                }
+            }
+        }
+        return res;
     }
 }
