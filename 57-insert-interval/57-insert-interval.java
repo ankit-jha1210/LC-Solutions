312e524a00cdeef1681076f0f3965e9971616c9c
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-08-27 20:37:10 +0530

    Time: 2 ms (82.53%), Space: 48.2 MB (12.78%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -1,42 +1,30 @@
 class Solution {
-
-    class Pair {
-        int first;
-        int second;
-
-        Pair(int first, int second) {
-            this.first = first;
-            this.second = second;
-        }
-    }
-
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        int n = intervals.length;
-        ArrayList<Pair> arr = new ArrayList<>();
-        for (int[] num : intervals) {
-            arr.add(new Pair(num[0], num[1]));
-        }
-        arr.add(new Pair(newInterval[0], newInterval[1]));
-        Collections.sort(arr, (a, b) -> a.first - b.first);
-        ArrayList<Pair> ans = new ArrayList<>();
-        int start = arr.get(0).first;
-        int end = arr.get(0).second;
-        for (int i = 0; i <= n; i++) {
-            if (end >= arr.get(i).first) {
-                end = Math.max(end, arr.get(i).second);
-            } else {
-                ans.add(new Pair(start, end));
-                start = arr.get(i).first;
-                end = arr.get(i).second;
+        ArrayList<int[]> ans = new ArrayList<>();
+        int idx = 0, n = intervals.length;
+        while(idx < n){
+            if(intervals[idx][0] < newInterval[0]){
+                ans.add(intervals[idx]);
+                idx++;
             }
+            else break;
         }
-        ans.add(new Pair(start, end));
-        int i = 0;
-        int[][] res = new int[ans.size()][2];
-        for (Pair p : ans) {
-            res[i][0] = p.first;
-            res[i++][1] = p.second;
+        
+        if(ans.size() == 0 || newInterval[0] > ans.get(ans.size() - 1)[1]){
+            ans.add(newInterval);
         }
-        return res;
+        else {
+           int[] lastInterval = ans.get(ans.size()-1);
+            lastInterval[1] = Math.max(lastInterval[1], newInterval[1]);
+        }
+        while(idx < n){
+            int[] lastInterval = ans.get(ans.size()-1);
+            if(lastInterval[1] >= intervals[idx][0]){
+                lastInterval[1] = Math.max(lastInterval[1], intervals[idx][1]);
+            }
+            else ans.add(intervals[idx]);
+            idx++;
+        }
+        return ans.toArray(new int[ans.size()][2]);
     }
 }
