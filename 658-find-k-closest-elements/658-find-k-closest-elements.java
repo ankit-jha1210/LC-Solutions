c89adc82081cde40ffe6c1f6926416d38b16933c
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-08-28 16:42:44 +0530

    Time: 83 ms (10.18%), Space: 68.1 MB (11.09%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,15 +1,29 @@
 class Solution {
 
+    class Pair {
+        int first, second;
+
+        Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int lo = 0, hi = arr.length - 1;
-        while (hi - lo >= k) {
-            if (arr[hi] - x >= x - arr[lo]) hi--; 
-            else lo++;
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> b.first - a.first);
+        int i = 0;
+        while (i < k) pq.add(new Pair(Math.abs(arr[i] - x), arr[i++]));
+        while (i < arr.length) {
+            int diff = Math.abs(arr[i] - x);
+            if (pq.peek().first > diff) {
+                pq.poll();
+                pq.add(new Pair(diff, arr[i]));
+            }
+            i++;
         }
-        List<Integer> res = new ArrayList<>();
-        for (int i = lo; i <= hi; i++) {
-            res.add(arr[i]);
-        }
-        return res;
+        List<Integer> lst = new ArrayList<>();
+        while (!pq.isEmpty()) lst.add(pq.poll().second);
+        Collections.sort(lst);
+        return lst;
     }
 }
