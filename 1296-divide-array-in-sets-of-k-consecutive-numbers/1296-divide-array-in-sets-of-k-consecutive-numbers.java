7578228b2edce482c643a1edb22b6c4dde980536
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-08-28 11:31:31 +0530

    Time: 211 ms (49.01%), Space: 78.7 MB (26.77%) - LeetHub

--- 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.java
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.java
@@ -2,20 +2,24 @@ class Solution {
 
     public boolean isPossibleDivide(int[] nums, int k) {
         if (nums.length % k != 0) return false;
-        Arrays.sort(nums);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
+        for (int num : map.keySet()) {
+            pq.add(num);
+        }
         int ans = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int occurences = map.get(nums[i]);
+        while (!pq.isEmpty()) {
+            int num = pq.poll();
+            int occurences = map.get(num);
             if (occurences == 0) continue;
             for (int j = 0; j < k; j++) {
-                if (!map.containsKey(nums[i] + j) || map.get(nums[i] + j) < occurences) {
+                if (!map.containsKey(num + j) || map.get(num + j) < occurences) {
                     return false;
                 } else {
-                    map.put(nums[i] + j, map.get(nums[i] + j) - occurences);
+                    map.put(num + j, map.get(num + j) - occurences);
                     ans += occurences;
                 }
             }
