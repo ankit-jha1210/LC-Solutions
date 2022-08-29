8dfe226fcfbdd9cd4eb37a7a8065d9a17166b606
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-08-29 22:00:33 +0530

    Time: 7 ms (31.88%), Space: 56.8 MB (70.48%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -5,7 +5,7 @@ class Solution {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == '1') {
-                    dfs(grid, i, j);
+                    bfs(grid, i, j);
                     islands++;
                 }
             }
@@ -13,12 +13,22 @@ class Solution {
         return islands;
     }
 
-    void dfs(char[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i == grid.length || j == grid[0].length || grid[i][j] == '0') return;
-        grid[i][j] = '0';
-        dfs(grid, i + 1, j);
-        dfs(grid, i, j + 1);
-        dfs(grid, i - 1, j);
-        dfs(grid, i, j - 1);
+    void bfs(char[][] grid, int r, int c) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] { r, c });
+
+        grid[r][c] = '0';
+        int[] dx = { 1, 0, -1, 0 };
+        int[] dy = { 0, 1, 0, -1 };
+        while (!q.isEmpty()) {
+            int[] dimensions = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int i1 = dx[i] + dimensions[0];
+                int j1 = dy[i] + dimensions[1];
+                if (i1 < 0 || j1 < 0 || i1 == grid.length || j1 == grid[0].length || grid[i1][j1] == '0') continue;
+                grid[i1][j1] = '0';
+                q.add(new int[] { i1, j1 });
+            }
+        }
     }
 }
