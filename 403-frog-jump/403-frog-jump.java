417b5cc868e8b8345eb16d5e4c2948d25236f953
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-09-02 11:27:17 +0530

    Time: 167 ms (14.50%), Space: 68.4 MB (57.19%) - LeetHub

--- 403-frog-jump/403-frog-jump.java
+++ 403-frog-jump/403-frog-jump.java
@@ -2,17 +2,16 @@ class Solution {
 
     public boolean canCross(int[] stones) {
         HashMap<Integer, HashSet<Integer>> map = new HashMap<>();
-        for (int i = 0; i < stones.length; i++) map.put(stones[i], new HashSet<>());
+        for (int stone : stones) map.put(stone, new HashSet<>());
         map.get(0).add(1);
-        for (int i = 0; i < stones.length; i++) {
-            HashSet<Integer> options = map.get(stones[i]);
-            for (int num : options) {
-                int pos = stones[i] + num;
-                if (pos == stones[stones.length - 1]) return true;
-                if (map.containsKey(pos)) {
-                    if (num - 1 > 0) map.get(pos).add(num - 1);
-                    map.get(pos).add(num);
-                    map.get(pos).add(num + 1);
+        for (int stone : stones) {
+            HashSet<Integer> jumps = map.get(stone);
+            for (int jump : jumps) {
+                if (jump + stone == stones[stones.length - 1]) return true;
+                if (map.containsKey(jump + stone)) {
+                    if (jump - 1 > 0) map.get(jump + stone).add(jump - 1);
+                    map.get(jump + stone).add(jump);
+                    map.get(jump + stone).add(jump + 1);
                 }
             }
         }
