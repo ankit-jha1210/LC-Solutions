239dd29da8db514302aa7cb2ed111aad44fdb427
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-09-05 19:38:42 +0530

    Time: 2 ms (55.15%), Space: 46.2 MB (49.76%) - LeetHub

---
+++ 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.java
@@ -0,0 +1,12 @@
+class Solution {
+    List<Integer> res = new ArrayList<>();
+
+    public List<Integer> postorder(Node root) {
+        if(root == null) return res;
+        for (Node child : root.children) {
+            postorder(child);
+        }
+        res.add(root.val);
+        return res;
+    }
+}
