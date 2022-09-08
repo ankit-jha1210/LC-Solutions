737acd82eeb92294e791b222c10c8cdfa29a3efb
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-09-08 16:11:06 +0530

    Time: 1 ms (89.88%), Space: 44.9 MB (6.11%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,8 +1,7 @@
 class Solution {
 
     public double myPow(double x, int n) {
-        if (n == 0) return 1;
-        if (x == 1) return 1;
+        if (n == 0 || x == 1) return 1;
         if (x == -1) {
             if (n % 2 == 0) return 1;
             return -1;
