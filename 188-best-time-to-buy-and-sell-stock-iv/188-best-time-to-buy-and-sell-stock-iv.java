8c41390bc1878bb8c7792a68d18ba8b7aea60b7f
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-09-10 09:36:23 +0530

    Time: 3 ms (76.36%), Space: 42 MB (80.54%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,20 +1,20 @@
 class Solution {
+
     public int maxProfit(int k, int[] prices) {
-        int[][] dp = new int[prices.length][2*k+1];
-        for(int[] row: dp) Arrays.fill(row, -1);
-        return solve(2*k, prices, 0, dp);
-    }
-    int solve(int transaction, int[] prices, int idx, int[][] dp) {
-        if(idx == prices.length || transaction == 0) return 0;
-        if(dp[idx][transaction] != -1) return dp[idx][transaction];
-        int notTake = solve(transaction, prices, idx+1, dp);
-        if(transaction % 2 == 0) {
-            int buy = solve(transaction-1, prices, idx+1, dp) - prices[idx];
-            return dp[idx][transaction] = Math.max(notTake, buy);
+        int n = prices.length;
+        int[][] dp = new int[n + 1][2 * k + 1];
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int transaction = 1; transaction <= 2 * k; transaction++) {
+                int notTake = dp[idx + 1][transaction];
+                if (transaction % 2 == 0) {
+                    int buy = dp[idx + 1][transaction - 1] - prices[idx];
+                    dp[idx][transaction] = Math.max(notTake, buy);
+                } else {
+                    int sell = dp[idx + 1][transaction - 1] + prices[idx];
+                    dp[idx][transaction] = Math.max(sell, notTake);
+                }
+            }
         }
-        else {
-            int sell = solve(transaction-1, prices, idx+1, dp) + prices[idx];
-            return dp[idx][transaction] = Math.max(sell, notTake);
-        }
+        return dp[0][2 * k];
     }
 }
