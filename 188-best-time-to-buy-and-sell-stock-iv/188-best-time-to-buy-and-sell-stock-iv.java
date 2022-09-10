5a51e87cf50e9cb64936269cdadee38592632e93
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-09-10 08:09:03 +0530

    Time: 20 ms (14.61%), Space: 45.6 MB (16.39%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,15 +1,17 @@
 class Solution {
+
     public int maxProfit(int k, int[] prices) {
-        int[][][] dp = new int[k+1][prices.length][2];
-        return helper(k, prices, 0, 1, dp);
-    }
-    int helper(int k, int[] prices, int idx, int canBuy, int[][][] dp) {
-        if(k == 0 || idx == prices.length) return 0;
-        if(dp[k][idx][canBuy] != 0) return dp[k][idx][canBuy];
-        int buy = 0, notBuy = 0;
-        if(canBuy == 1) buy = -prices[idx] + helper(k, prices, idx+1, 0, dp);
-        else notBuy =  helper(k-1, prices, idx+1, 1, dp) + prices[idx];
-        return dp[k][idx][canBuy] = Math.max(helper(k, prices, idx+1, canBuy, dp), Math.max(buy, notBuy));
-         
+        int n = prices.length;
+        int[][][] dp = new int[n+1][k + 1][2];
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int j = 1; j <= k; j++) {
+                for (int canBuy = 0; canBuy <= 1; canBuy++) {
+                    int notTake = dp[idx + 1][j][canBuy];
+                    if (canBuy == 1) dp[idx][j][canBuy] = Math.max(-prices[idx] + dp[idx + 1][j][0], notTake); 
+                    else dp[idx][j][canBuy] = Math.max(dp[idx + 1][j - 1][1] + prices[idx], notTake);
+                }
+            }
+        }
+        return dp[0][k][1];
     }
 }
