fda5768431784fd6a75d1adefa389b8322481988
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-09-15 10:46:31 +0530

    Time: 45 ms (76.56%), Space: 51.7 MB (92.34%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.java
+++ 904-fruit-into-baskets/904-fruit-into-baskets.java
@@ -1,20 +1,23 @@
 class Solution {
-
     public int totalFruit(int[] fruits) {
+        HashMap<Integer, Integer> map = new HashMap<>();
         int i = 0, j = 0;
         int maxLen = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        while (j < fruits.length) {
+        while(j < fruits.length) {
             map.put(fruits[j], map.getOrDefault(fruits[j], 0) + 1);
-            if (map.size() <= 2) {
+            if(map.size() <= 2) {
                 maxLen = Math.max(maxLen, j - i + 1);
+                j++;
             }
-            while (map.size() > 2) {
-                int occurence = map.get(fruits[i]) - 1;
-                if (occurence == 0) map.remove(fruits[i]); else map.put(fruits[i], occurence);
-                i++;
+            else {
+                while(map.size() > 2) {
+                    int occurence = map.get(fruits[i]) - 1;
+                    if(occurence == 0) map.remove(fruits[i]);
+                    else map.put(fruits[i], occurence);
+                    i++;
+                }
+                j++;
             }
-            j++;
         }
         return maxLen;
     }
