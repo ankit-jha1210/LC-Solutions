a44351a1cd2fba55bc1d2ddebc3221f0dfd4257f
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-09-15 14:51:40 +0530

    Time: 16 ms (82.92%), Space: 106.1 MB (70.80%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
@@ -1,27 +1,14 @@
 class Solution {
     public int pseudoPalindromicPaths (TreeNode root) {
-        return helper(root, new int[10]);
+        return helper(root, 0);
     }
-    int helper(TreeNode root, int[] freq) {
+    int helper(TreeNode root, int path) {
         if(root == null) return 0;
-        freq[root.val]++;
+        path = path ^ (1 << root.val);
         if(root.left == null && root.right == null) {
-            int ans = isPalindrome(freq);
-            freq[root.val]--;
-            return ans;
+            if((path & path-1) == 0) return 1;
+            return 0;
         }
-        int ans = helper(root.left, freq) + helper(root.right, freq);
-        freq[root.val]--;
-        return ans;
-    }
-    int isPalindrome(int[] freq) {
-        int cnt = 0;
-        for(int i: freq) {
-            if(i % 2 == 1) {
-                cnt++;
-                if(cnt > 1) return 0;
-            }
-        }
-        return 1;
+        return helper(root.left, path) + helper(root.right, path);
     }
 }
