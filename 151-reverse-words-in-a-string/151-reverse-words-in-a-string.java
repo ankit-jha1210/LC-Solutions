16a7929cb689eeb917e5777426381cb1de58ac7e
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-09-16 10:24:05 +0530

    Time: 19 ms (36.42%), Space: 54.4 MB (18.69%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -1,21 +1,18 @@
 class Solution {
 
     public String reverseWords(String s) {
-        Stack<String> stk = new Stack<>();
-        String temp = "", ans = "";
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ' ') {
-                if (temp.length() > 0) {
-                    stk.push(temp);
-                    temp = "";
-                }
-            } else {
-                temp = temp + s.charAt(i);
-            }
+        int i = s.length() - 1;
+        String ans = "";
+        while (i >= 0) {
+            // removing trailing spaces
+            while (i >= 0 && s.charAt(i) == ' ') i--;
+            if (i < 0) break;
+            int j = i;
+            // finding starting of a word
+            while (i >= 0 && s.charAt(i) != ' ') i--;
+            if (ans.length() == 0) ans = ans.concat(s.substring(i + 1, j + 1)); 
+            else ans = ans.concat(" " + s.substring(i + 1, j + 1));
         }
-        ans += temp;
-        while (!stk.isEmpty()) ans = ans + " "+ stk.pop();
-        while (ans.length() != 0 && ans.charAt(0) == ' ') ans = ans.substring(1);
         return ans;
     }
 }
