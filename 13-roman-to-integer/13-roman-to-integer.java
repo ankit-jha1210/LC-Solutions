244dfb7d58e64c31610dc7183c68aee30206f1f3
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-09-16 11:15:07 +0530

    Time: 14 ms (25.52%), Space: 46.4 MB (44.36%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,55 +1,19 @@
 class Solution {
 
     public int romanToInt(String s) {
-        int i = 0;
-        int res = 0;
-        int n = s.length();
-        while (i < n) {
-            if (s.charAt(i) == 'M') res += 1000; 
-            else if (s.charAt(i) == 'D') res += 500; 
-            else if(s.charAt(i) == 'L') res += 50;
-            else if(s.charAt(i) == 'V') res += 5;
-            else if (s.charAt(i) == 'C') {
-                if (i + 1 < n) {
-                    if (s.charAt(i + 1) == 'D') {
-                        res += 400;
-                        i += 2;
-                        continue;
-                    } else if (s.charAt(i + 1) == 'M') {
-                        res += 900;
-                        i += 2;
-                        continue;
-                    }
-                }
-                res += 100;
-            } else if (s.charAt(i) == 'X') {
-                if (i + 1 < n) {
-                    if (s.charAt(i + 1) == 'L') {
-                        res += 40;
-                        i += 2;
-                        continue;
-                    } else if (s.charAt(i + 1) == 'C') {
-                        res += 90;
-                        i += 2;
-                        continue;
-                    }
-                }
-                res += 10;
-            } else if (s.charAt(i) == 'I') {
-                if (i + 1 < n) {
-                    if (s.charAt(i + 1) == 'V') {
-                        res += 4;
-                        i += 2;
-                        continue;
-                    } else if (s.charAt(i + 1) == 'X') {
-                        res += 9;
-                        i += 2;
-                        continue;
-                    }
-                }
-                res += 1;
-            }
-            i++;
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int res = map.get(s.charAt(s.length() - 1));
+        for (int i = s.length() - 2; i >= 0; i--) {
+            if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                res -= map.get(s.charAt(i));
+            } else res += map.get(s.charAt(i));
         }
         return res;
     }
