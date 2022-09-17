61c180c24cee5eb7c07942250bab538a6be32d09
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-09-17 09:52:41 +0530

    Time: 63 ms (84.55%), Space: 95.4 MB (85.10%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -2,16 +2,18 @@ class Solution {
 
     public int maximumScore(int[] nums, int[] muls) {
         int m = muls.length, n = nums.length;
-        int[][] dp = new int[m + 1][m + 1];
+        int[] fwd = new int[m + 1];
         for (int l = m - 1; l >= 0; l--) {
+            int[] curr = new int[m + 1];
             for (int i = m - 1; i >= 0; i--) {
                 if (l > i) break;
-                int left = dp[l + 1][i + 1] + nums[l] * muls[i];
-                int right = dp[l][i + 1] + nums[n - (i - l) - 1] * muls[i];
-                dp[l][i] = Math.max(left, right);
+                int left = fwd[i + 1] + nums[l] * muls[i];
+                int right = curr[i + 1] + nums[n - (i - l) - 1] * muls[i];
+                curr[i] = Math.max(left, right);
             }
+            fwd = curr;
         }
-        return dp[0][0];
+        return fwd[0];
     }
     // int solve(int[] nums, int[] muls, int l, int i, Integer[][] dp) {
     //     if (l == nums.length || i == muls.length) return 0;
