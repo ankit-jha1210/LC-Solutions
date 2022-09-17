e010a1933c95e0ff673de9ddf687b5aa671ad336
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-09-17 09:50:10 +0530

    Time: 65 ms (82.93%), Space: 101.6 MB (76.15%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -1,15 +1,23 @@
 class Solution {
 
     public int maximumScore(int[] nums, int[] muls) {
-        Integer[][] dp = new Integer[muls.length][muls.length];
-        return solve(nums, muls, 0, 0, dp);
+        int m = muls.length, n = nums.length;
+        int[][] dp = new int[m + 1][m + 1];
+        for (int l = m - 1; l >= 0; l--) {
+            for (int i = m - 1; i >= 0; i--) {
+                if (l > i) break;
+                int left = dp[l + 1][i + 1] + nums[l] * muls[i];
+                int right = dp[l][i + 1] + nums[n - (i - l) - 1] * muls[i];
+                dp[l][i] = Math.max(left, right);
+            }
+        }
+        return dp[0][0];
     }
-
-    int solve(int[] nums, int[] muls, int i, int l, Integer[][] dp) {
-        if (i == muls.length || l == nums.length) return 0;
-        if(dp[i][l] != null) return dp[i][l];
-        int leftPicked = solve(nums, muls, i + 1, l + 1, dp) + nums[l] * muls[i];
-        int rightPicked = solve(nums, muls, i + 1, l, dp) + nums[nums.length - (i - l) - 1] * muls[i];
-        return dp[i][l] =  Math.max(leftPicked, rightPicked);
-    }
+    // int solve(int[] nums, int[] muls, int l, int i, Integer[][] dp) {
+    //     if (l == nums.length || i == muls.length) return 0;
+    //     if (dp[l][i] != null) return dp[l][i];
+    //     int left = solve(nums, muls, l + 1, i + 1, dp) + nums[l] * muls[i];
+    //     int right = solve(nums, muls, l, i + 1, dp) + nums[nums.length - (i - l) - 1] * muls[i];
+    //     return dp[l][i] = Math.max(left, right);
+    // }
 }
