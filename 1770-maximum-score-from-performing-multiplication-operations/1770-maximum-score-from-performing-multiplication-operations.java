5484dd6b9442e1527876b114014c4f9e8fde9302
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-09-17 09:52:52 +0530

    Time: 73 ms (75.25%), Space: 98.4 MB (81.12%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -15,11 +15,4 @@ class Solution {
         }
         return fwd[0];
     }
-    // int solve(int[] nums, int[] muls, int l, int i, Integer[][] dp) {
-    //     if (l == nums.length || i == muls.length) return 0;
-    //     if (dp[l][i] != null) return dp[l][i];
-    //     int left = solve(nums, muls, l + 1, i + 1, dp) + nums[l] * muls[i];
-    //     int right = solve(nums, muls, l, i + 1, dp) + nums[nums.length - (i - l) - 1] * muls[i];
-    //     return dp[l][i] = Math.max(left, right);
-    // }
 }
