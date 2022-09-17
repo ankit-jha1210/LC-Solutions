8588523a4fd001ee403104ad4a648b6a8cfbb4d5
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-09-17 11:05:25 +0530

    Time: 34 ms (59.38%), Space: 59.6 MB (37.41%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -3,7 +3,6 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        HashSet<List<Integer>> set = new HashSet<>();
         for (int idx = 0; idx < nums.length - 1; idx++) {
             int i = idx + 1, j = nums.length - 1;
             while (i < j) {
@@ -13,10 +12,9 @@ class Solution {
                     lst.add(nums[idx]);
                     lst.add(nums[i]);
                     lst.add(nums[j]);
-                    if (!set.contains(lst)) {
-                        set.add(lst);
-                        res.add(lst);
-                    }
+                    res.add(lst);
+                    while(i+1<nums.length && nums[i+1] == nums[i]) i++;
+                    while(j-1 >= 0 && nums[j] == nums[j-1]) j--;
                     i++;
                     j--;
                 } else if (sum > 0) j--; 
