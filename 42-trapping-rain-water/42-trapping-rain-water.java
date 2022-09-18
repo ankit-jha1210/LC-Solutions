e57c8c145ec7f3b89474251139159a46b79741e5
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-09-18 23:04:35 +0530

    Time: 1 ms (99.76%), Space: 49.1 MB (34.50%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -6,7 +6,12 @@ class Solution {
         int leftMax = 0, rightMax = 0;
         while (left <= right) {
             if (height[left] <= height[right]) {
-                if (height[left] >= leftMax) leftMax = height[left]; else ans += leftMax - height[left];
+                // we come here which means something is greater in right than it
+                // and how can we be sure that leftMax <= rightMax
+                // well we move the left pointer only if it is <= height[right]
+                // so there is someone on right greater than it and in question we require min
+                if (height[left] >= leftMax) leftMax = height[left]; 
+                else ans += leftMax - height[left];
                 left++;
             } else {
                 if (height[right] >= rightMax) rightMax = height[right]; else ans += rightMax - height[right];
