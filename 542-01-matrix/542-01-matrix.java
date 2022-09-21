c7df61d30ca486bb5fac43afe980778530de44a1
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-09-21 16:28:08 +0530

    Time: 39 ms (17.41%), Space: 73.2 MB (37.37%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,32 +1,35 @@
 class Solution {
-    int[] dx = { -1, 1, 0, 0 };
-    int[] dy = { 0, 0, -1, 1 };
-
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, -1, 1};
     public int[][] updateMatrix(int[][] mat) {
-        int m = mat.length;
-        int n = mat[0].length;
-        int[][] res = new int[m][n];
+        int m = mat.length, n = mat[0].length;
         boolean[][] visited = new boolean[m][n];
         Queue<int[]> q = new LinkedList<>();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 0) {
-                    q.add(new int[] { i, j, 0 });
+        for(int i = 0;i<m;i++) {
+            for(int j = 0;j<n;j++) {
+                if(mat[i][j] == 0) {
+                    q.add(new int[] {i, j});
                     visited[i][j] = true;
                 }
             }
         }
-        while (!q.isEmpty()) {
-            int[] cell = q.poll();
-            int r = cell[0], c = cell[1], steps = cell[2];
-            res[r][c] = steps;
-            for (int i = 0; i < 4; i++) {
-                if (r + dx[i] >= 0 && r + dx[i] < m && c + dy[i] >= 0 && c + dy[i] < n && !visited[r + dx[i]][c + dy[i]]) {
-                    q.add(new int[] { r + dx[i], c + dy[i], steps + 1 });
-                    visited[r + dx[i]][c + dy[i]] = true;
+        int dist = 0;
+        int[][] res = new int[m][n];
+        while(!q.isEmpty()) {
+            int size = q.size();
+            for(int i = 0;i<size;i++) {
+                int[] coordinate = q.poll();
+                int x = coordinate[0];
+                int y = coordinate[1];
+                res[x][y] = dist;
+                for(int j = 0;j<4;j++) {
+                    if(x + dx[j] < 0 || x + dx[j] == m || y + dy[j] < 0 || y + dy[j] == n || visited[x+dx[j]][y + dy[j]]) continue;
+                    q.add(new int[] {dx[j] + x, dy[j]+y});
+                    visited[dx[j]+x][dy[j]+y] = true;
                 }
             }
+                dist++;
         }
-        return res;
+            return res;
     }
 }
