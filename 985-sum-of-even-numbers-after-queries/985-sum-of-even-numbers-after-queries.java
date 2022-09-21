213c34da48a36aec0ad774edd2aa8ad913ba6c66
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-09-21 10:43:22 +0530

    Time: 12 ms (18.01%), Space: 76 MB (6.11%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
@@ -8,13 +8,11 @@ class Solution {
         }
         for (int i = 0; i < queries.length; i++) {
             int idx = queries[i][1];
-            int val = queries[i][0];
-            int tmp = nums[idx] + val;
             if (nums[idx] % 2 == 0) {
                 sum -= nums[idx];
             }
-            nums[idx] = tmp;
-            if (nums[idx] % 2 == 0) sum += tmp;
+            nums[idx] += queries[i][0];
+            if (nums[idx] % 2 == 0) sum += nums[idx];
             res[i] = sum;
         }
         return res;
