b7fbca20d1f069af13918c91015282461ef5b99e
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-09-22 10:57:02 +0530

    Time: 1 ms (42.25%), Space: 41.2 MB (76.33%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -9,17 +9,16 @@ class Solution {
             if (i != 0) lst1.add(nums[i]);
             if (i != n - 1) lst2.add(nums[i]);
         }
-        Integer[] dp = new Integer[n];
-        return Math.max(solve(lst1, dp), solve(lst2, dp));
+        return Math.max(solve(lst1), solve(lst2));
     }
 
-    int solve(List<Integer> nums, Integer[] dp) {
-        dp[nums.size()] = 0;
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            int pick = nums.get(i);
-            if (i + 2 < nums.size()) pick += dp[i + 2];
-            dp[i] = Math.max(dp[i + 1], pick);
+    int solve(List<Integer> nums) {
+        int prev1 = nums.get(nums.size() - 1), prev2 = 0;
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            int curr_i = Math.max(prev2 + nums.get(i), prev1);
+            prev2 = prev1;
+            prev1 = curr_i;
         }
-        return dp[0];
+        return prev1;
     }
 }
