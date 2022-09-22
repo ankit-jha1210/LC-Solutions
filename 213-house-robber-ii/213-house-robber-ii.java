e86ff9a6dcb405172e52ef21d3bb37acce50c792
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-09-22 10:54:09 +0530

    Time: 2 ms (6.63%), Space: 41.1 MB (79.50%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -2,24 +2,24 @@ class Solution {
 
     public int rob(int[] nums) {
         int n = nums.length;
-        if(n == 1) return nums[0];
+        if (n == 1) return nums[0];
         List<Integer> lst1 = new ArrayList<>();
         List<Integer> lst2 = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
             if (i != 0) lst1.add(nums[i]);
             if (i != n - 1) lst2.add(nums[i]);
         }
-        Integer[] dp1 = new Integer[n];
-        Integer[] dp2 = new Integer[n];
-        return Math.max(solve(lst1, 0, dp1), solve(lst2, 0, dp2));
+        Integer[] dp = new Integer[n];
+        return Math.max(solve(lst1, dp), solve(lst2, dp));
     }
 
-    int solve(List<Integer> nums, int i, Integer[] dp) {
-        if (i == nums.size()) return 0;
-        if (dp[i] != null) return dp[i];
-        int pick = nums.get(i);
-        if (i + 2 < nums.size()) pick += solve(nums, i + 2, dp);
-        int notPick = solve(nums, i + 1, dp);
-        return dp[i] = Math.max(pick, notPick);
+    int solve(List<Integer> nums, Integer[] dp) {
+        dp[nums.size()] = 0;
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            int pick = nums.get(i);
+            if (i + 2 < nums.size()) pick += dp[i + 2];
+            dp[i] = Math.max(dp[i + 1], pick);
+        }
+        return dp[0];
     }
 }
