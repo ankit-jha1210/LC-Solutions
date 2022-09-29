57688324b314b19d8e4e90ff49e2c5acd3b30f7d
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-09-29 10:32:26 +0530

    Time: 88 ms (11.30%), Space: 68.3 MB (9.61%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,29 +1,23 @@
 class Solution {
 
-    class Pair {
-        int first, second;
-
-        Pair(int first, int second) {
-            this.first = first;
-            this.second = second;
-        }
-    }
-
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> b.first - a.first);
-        int i = 0;
-        while (i < k) pq.add(new Pair(Math.abs(arr[i] - x), arr[i++]));
-        while (i < arr.length) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        for (int i = 0; i < arr.length; i++) {
             int diff = Math.abs(arr[i] - x);
-            if (pq.peek().first > diff) {
-                pq.poll();
-                pq.add(new Pair(diff, arr[i]));
+            if (i < k) {
+                pq.add(new int[] { diff, arr[i] });
+            } else {
+                if (diff < pq.peek()[0]) {
+                    pq.poll();
+                    pq.add(new int[] { diff, arr[i] });
+                }
             }
-            i++;
         }
-        List<Integer> lst = new ArrayList<>();
-        while (!pq.isEmpty()) lst.add(pq.poll().second);
-        Collections.sort(lst);
-        return lst;
+        List<Integer> res = new ArrayList<>();
+        while (!pq.isEmpty()) {
+            res.add(pq.poll()[1]);
+        }
+        Collections.sort(res);
+        return res;
     }
 }
