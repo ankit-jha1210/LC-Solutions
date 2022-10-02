9b6c7b02de0d890d005a704bb99f043415d2a246
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-10-02 22:11:57 +0530

    Time: 19 ms (83.17%), Space: 42.8 MB (46.45%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,18 +1,19 @@
 class Solution {
-    final int mod = (int)(Math.pow(10, 9)) + 7;
+    final int mod = (int)Math.pow(10, 9) + 7;
     public int numRollsToTarget(int n, int k, int target) {
-        Integer[][][] dp = new Integer[n + 1][k + 1][target + 1];
-        return solve(n, k, k, target, dp);
+        Integer[][] dp = new Integer[n + 1][target + 1];
+        return solve(n, k, target, dp);
     }
-    int solve(int n, int k, int no_left, int target, Integer[][][] dp) {
-        if(target == 0 && n == 0) return 1;
-        if(n == 0 || target == 0 || no_left == 0) return 0;
-        if(dp[n][no_left][target] != null) return dp[n][no_left][target];
-         int pick = 0;
-        if(target >= no_left) {
-            pick = solve(n - 1, k, k, target - no_left, dp);
+
+    int solve(int n, int k, int target, Integer[][] dp) {
+        if (n == 0 && target == 0) return 1;
+        if (target == 0 || n == 0) return 0;
+        if (dp[n][target] != null) return dp[n][target];
+        int ans = 0;
+        for (int i = 1; i <= k; i++) {
+            if (i > target) break;
+            ans = (ans + solve(n - 1, k, target - i, dp)) % mod;
         }
-        int notPick = solve(n, k,  no_left - 1, target, dp);
-        return dp[n][no_left][target] =  (pick + notPick) % mod;
+        return dp[n][target] = ans;
     }
 }
