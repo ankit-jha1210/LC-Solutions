22400fc5b750f300b82bfec70ce89db93fb9432f
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-10-02 22:18:07 +0530

    Time: 34 ms (54.77%), Space: 41.1 MB (92.71%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -2,18 +2,20 @@ class Solution {
     final int mod = (int) Math.pow(10, 9) + 7;
 
     public int numRollsToTarget(int n, int k, int target) {
-        int[][] dp = new int[n + 1][target + 1];
-        dp[0][0] = 1;
+        int[] prev = new int[target + 1];
+        prev[0] = 1;
         for (int i = 1; i <= n; i++) {
+            int[] curr = new int[target + 1];
             for (int j = 1; j <= target; j++) {
                 int ans = 0;
                 for (int idx = 1; idx <= k; idx++) {
                     if (idx > j) break;
-                    ans = (ans + dp[i - 1][j - idx]) % mod;
+                    ans = (ans + prev[j - idx]) % mod;
                 }
-                dp[i][j] = ans;
+                curr[j] = ans;
             }
+            prev = curr;
         }
-        return dp[n][target];
+        return prev[target];
     }
 }
