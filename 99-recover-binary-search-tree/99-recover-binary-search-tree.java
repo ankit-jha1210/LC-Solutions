f37f79665f2152773a7107ed95af1fd06e478bd4
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-10-04 11:28:27 +0530

    Time: 3 ms (84.98%), Space: 47.1 MB (62.52%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -1,27 +1,22 @@
 class Solution {
-    //  brute force we are just sorting the array and comparing with tree
-    int i = 0;
+    TreeNode first, second, prev;
+
     public void recoverTree(TreeNode root) {
-        List<Integer> lst = new ArrayList<>();
-        inorder(root, lst);
-        Collections.sort(lst);
-        check(root, lst);
+        prev = new TreeNode(Integer.MIN_VALUE);
+        inorder(root);
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
     }
-    void inorder(TreeNode root, List<Integer> lst) {
-        if(root == null) return;
-        inorder(root.left, lst);
-        lst.add(root.val);
-        inorder(root.right, lst);
-    }
-    void check(TreeNode root, List<Integer> lst) {
-        if(root == null) return;
-        check(root.left, lst);
-        if(root.val != lst.get(i)) {
-            root.val = lst.get(i);
+
+    void inorder(TreeNode root) {
+        if (root == null) return;
+        inorder(root.left);
+        if (prev.val > root.val) {
+            if (first == null) first = prev;
+            second = root;
         }
-        i++;
-        check(root.right, lst);
-        
-        
+        prev = root;
+        inorder(root.right);
     }
 }
