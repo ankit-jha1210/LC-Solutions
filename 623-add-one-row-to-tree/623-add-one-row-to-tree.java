8321e7afb2735b41a31144c80acbd10476f3f4f9
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-10-05 19:22:13 +0530

    Time: 1 ms (88.59%), Space: 45.4 MB (54.38%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.java
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.java
@@ -1,26 +1,32 @@
 class Solution {
 
     public TreeNode addOneRow(TreeNode root, int val, int depth) {
-        if(depth == 1) {
+        if (depth == 1) {
             TreeNode node = new TreeNode(val);
             node.left = root;
             return node;
         }
-        insert(root, val, depth, 1);
-        return root;
-    }
-    void insert(TreeNode root, int val, int d, int curr_Depth) {
-        if(root == null) return;
-        if(d - 1 == curr_Depth) {
-            TreeNode node = new TreeNode(val);
-            node.left = root.left;
-            root.left = node;
-            node = new TreeNode(val);
-            node.right = root.right;
-            root.right = node;
-            return;
+        Queue<TreeNode> q = new LinkedList<>();
+        int curr_Depth = 1;
+        q.add(root);
+        while (curr_Depth < depth - 1) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if (node.left != null) q.add(node.left);
+                if (node.right != null) q.add(node.right);
+            }
+            curr_Depth++;
         }
-        insert(root.left, val, d, curr_Depth + 1);
-        insert(root.right, val, d, curr_Depth + 1);
+        while (!q.isEmpty()) {
+            TreeNode frontNode = q.poll();
+            TreeNode node = new TreeNode(val);
+            node.left = frontNode.left;
+            frontNode.left = node;
+            node = new TreeNode(val);
+            node.right = frontNode.right;
+            frontNode.right = node;
+        }
+        return root;
     }
 }
