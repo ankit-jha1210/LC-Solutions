5b7bbcdfd26ccc68dbd37fc4291ba153e050b2b1
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-10-09 15:29:28 +0530

    Time: 14 ms (10.51%), Space: 49.8 MB (80.86%) - LeetHub

---
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public boolean findTarget(TreeNode root, int k) {
+        return findTarget(root, root, k);
+    }
+
+    boolean findTarget(TreeNode root, TreeNode curr, int k) {
+        if (curr == null) return false;
+        return find(root, curr, k - curr.val) || 
+              findTarget(root, curr.left, k) || 
+              findTarget(root, curr.right, k);
+    }
+
+    boolean find(TreeNode root, TreeNode curr, int k) {
+        if (root == null) return false;
+        if (root.val == k && root != curr) return true;
+        return find(root.left, curr, k) || find(root.right, curr, k);
+    }
+}
