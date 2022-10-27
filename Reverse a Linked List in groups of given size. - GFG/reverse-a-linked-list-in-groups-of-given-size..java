3d7f5af34f91cd7ba5b7e389839f9da0587744d1
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-10-27 13:06:13 +0530

    Added solution - LeetHub

---
+++ Reverse a Linked List in groups of given size. - GFG/reverse-a-linked-list-in-groups-of-given-size..java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class ReverseInSize
+{
+    static Node head;
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node tail = head;
+            for(int i = 1; i < n; i++)
+            {
+                int a = sc.nextInt();
+                // addToTheLast(new Node(a));
+                tail.next = new Node(a);
+                tail =tail.next;
+            }
+            
+            int k = sc.nextInt();
+            Solution ob = new Solution();
+            Node res = ob.reverse(head, k);
+            printList(res);
+            System.out.println();
+        }
+    }
+    
+    public static void printList(Node node)
+    {
+        while(node != null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+    
+}
+
+
+// } Driver Code Ends
+class Solution
+{
+    public static Node reverse(Node head, int k)
+    {
+        if(head == null || head.next == null || k == 0) return head;
+        Node curr = head, prev = null, fwd = null;
+        for(int i = 0; i < k && curr != null; i++) {
+            fwd = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = fwd;
+        }
+        if(curr != null) head.next = reverse(curr, k);
+        return prev;
+        
+    }
+}
+
