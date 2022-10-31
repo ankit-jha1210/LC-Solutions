e3b314ca948d7d938540f0449d2429e6e77b5bf4
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2022-10-31 10:34:19 +0530

    Time: 1 ms (93.83%), Space: 46.4 MB (8.14%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -1,10 +1,9 @@
 class Solution {
+
     public boolean isToeplitzMatrix(int[][] matrix) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[0].length; j++) {
-                if(!map.containsKey(i - j)) map.put(i - j, matrix[i][j]);
-                else if(map.get(i - j) != matrix[i][j]) return false;
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] != matrix[i - 1][j - 1]) return false;
             }
         }
         return true;
