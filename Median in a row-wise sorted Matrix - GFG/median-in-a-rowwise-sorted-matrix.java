bc1fa74770a7a6c5bd71b520b6bbfa99aa546e0a
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2023-03-24 11:33:07 +0530

    Added solution - LeetHub

---
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String S[] = read.readLine().split(" ");
+            int R = Integer.parseInt(S[0]);
+            int C = Integer.parseInt(S[1]);
+            int matrix[][] = new int[R][C];
+            int c = 0;
+            for(int i = 0; i < R; i++){
+                String line[]=read.readLine().trim().split(" ");
+                for(int j = 0; j < C; j++){
+                    matrix[i][j] = Integer.parseInt(line[j]);
+                }
+            }
+            Solution ob = new Solution();
+            int ans = ob.median(matrix, R, C);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Solution {
+    int median(int matrix[][], int R, int C) {
+        int low = 1, high = 2000;
+        int size = R * C;
+        int ans = -1;
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            
+            int total = 0;
+            for(int i = 0; i < R; i++) {
+                total += ceil(matrix[i], mid);
+            }
+            if(total > size/2) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+        return ans;
+    }
+    
+    int ceil(int[] arr, int num) {
+        int low = 0, high = arr.length - 1;
+        while(low <= high) {
+              int mid = low + (high - low)/2;
+            if(arr[mid] <= num) low = mid + 1;
+            else high = mid - 1;
+        }
+        return low;
+    }
+    
+    
+}
