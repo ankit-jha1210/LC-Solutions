b36b36d486d5ca5bc40aa478cead1c2f46a1452d
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2023-04-02 09:23:38 +0530

    Added solution - LeetHub

--- Interleaved Strings - GFG/interleaved-strings.java
+++ Interleaved Strings - GFG/interleaved-strings.java
@@ -23,27 +23,30 @@ class InterLeaveString
 
 
 // } Driver Code Ends
+
+
+/*you are required to complete this method */
 class Solution {
 	public boolean isInterLeave(String a,String b,String c)
 	{
-	    Boolean[][]dp = new Boolean[a.length() + 1][b.length() + 1];
+	    int m = a.length(), n = b.length();
+	    Boolean[][] dp = new Boolean[m + 1][n + 1];
+	    if((m + n) != c.length()) return false;
             return solve(a, b, c, 0, 0, dp);
 	}
 	
-	boolean solve(String a, String b, String c, int i, int j, Boolean[][]dp) {
+	boolean solve(String a, String b, String c, int i, int j, Boolean[][] dp) {
 	    if(i == a.length() && j == b.length()) return true;
 	    
 	    if(dp[i][j] != null) return dp[i][j];
-	    
 	    if(i < a.length() && j < b.length()) {
-	        if(a.charAt(i) == b.charAt(j) && a.charAt(i) == c.charAt(i + j)) {
-	            return dp[i][j] =  solve(a, b, c, i + 1, j, dp) || solve(a, b, c, i,j + 1,  dp);
-	        }
+	    if(a.charAt(i) == b.charAt(j) && b.charAt(j) == c.charAt(i + j)) {
+	        return dp[i][j] =  solve(a, b, c, i + 1, j, dp) || solve(a, b, c, i, j + 1, dp);
 	    }
-	    
-	    if(i < a.length() && c.charAt(i + j) == a.charAt(i)) return dp[i][j] = solve(a, b, c, i + 1, j, dp);
-	    else if(j < b.length() && c.charAt(i + j) == b.charAt(j)) return dp[i][j] = solve(a, b, c, i,j + 1,dp);
-	    else return dp[i][j] = false;
+	    }
+	    if(i < a.length() && a.charAt(i) == c.charAt(i + j)) return dp[i][j] = solve(a, b, c, i + 1, j, dp);
+	    else if(j < b.length() && b.charAt(j) == c.charAt(i + j)) return dp[i][j] =  solve(a, b, c, i, j + 1, dp);
+	    else return dp[i][j] =  false;
 	}
 }
 
