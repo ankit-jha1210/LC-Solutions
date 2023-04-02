b271737a79685618c37089490e8ba36ae97ec0ca
Ankit Jha <73438654+J-Ankit2020@users.noreply.github.com>
2023-04-02 09:46:58 +0530

    Added solution - LeetHub

--- Edit Distance - GFG/edit-distance.java
+++ Edit Distance - GFG/edit-distance.java
@@ -25,19 +25,20 @@ class GFG {
 class Solution {
     public int editDistance(String s, String t) {
         
-        Integer[][] dp = new Integer[s.length()][t.length()];
-        return solve(s, t, s.length() - 1, t.length() - 1, dp);
+        Integer[][] dp = new Integer[s.length() + 1][t.length() + 1];
+        
+        return solve(s, t, s.length(), t.length(), dp);
     }
     
     int solve(String s, String t, int i, int j, Integer[][] dp) {
         
-        if(i < 0 && j < 0) return 0;
-        if(i < 0) return  j + 1;
-        if(j < 0) return i + 1;
+        if(i == 0 && j == 0) return 0;
+        if(i == 0) return  j;
+        if(j == 0) return i;
         
         if(dp[i][j] != null) return dp[i][j];
         
-        if(s.charAt(i) == t.charAt(j)) return solve(s, t, i - 1, j - 1, dp);
+        if(s.charAt(i - 1) == t.charAt(j - 1)) return solve(s, t, i - 1, j - 1, dp);
         
         int op1 = solve(s, t, i , j - 1, dp);
         int op2 = solve(s, t, i - 1, j, dp);
