c46b77e7e31cda66e4b9dbf36d8f6447a620d308
Ankit Jha <jhaa11701@gmail.com>
2023-07-30 08:44:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (10.84%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        dfs(root, res, 0);
+        return res;
+    }
+
+    void dfs(TreeNode root, List<Integer> res, int level) {
+        if (root == null) return;
+
+        if (level == res.size()) res.add(root.val);
+        dfs(root.right, res, level + 1);
+        dfs(root.left, res, level + 1);
+    }
+}
