5f70bb403a35c038812751f0726accc4854c25ed
Ankit Jha <jhaa11701@gmail.com>
2023-07-31 19:12:22 +0530

    Time: 55 ms (82.67%) | Memory: 58.2 MB (22.22%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.java
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.java
@@ -8,22 +8,20 @@ class Solution {
         for (String key : map.keySet()) {
             int freq = map.get(key);
             if (key.charAt(0) == key.charAt(1)) {
-                System.out.println("hello1" + key);
-                if (freq % 2 == 0) cnt += 2 * freq; else {
+                if (freq % 2 == 0) cnt += 2 * freq; 
+                else {
                     cnt += 2 * (freq - 1);
                     isCentral = true;
                 }
             } else {
                 String rev = key.charAt(1) + "" + key.charAt(0);
                 if (map.containsKey(rev)) {
-                    System.out.println("hello" + 2);
                     int min = Math.min(freq, map.get(rev));
                     cnt += 4 * min;
                 }
             }
             map.put(key, 0);
         }
-        System.out.println(isCentral);
         if (isCentral) cnt += 2;
         return cnt;
     }
