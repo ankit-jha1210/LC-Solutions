35664968d9f11bccb692c62e745a932a3d1b27a0
Ankit Jha <jhaa11701@gmail.com>
2023-08-14 10:28:34 +0530

    Time: 15 ms (18.27%) | Memory: 44.5 MB (14.15%) - LeetSync

--- 696-count-binary-substrings/count-binary-substrings.java
+++ 696-count-binary-substrings/count-binary-substrings.java
@@ -1,18 +1,27 @@
 class Solution {
 
     public int countBinarySubstrings(String s) {
-        List<Integer> temp = new ArrayList<>();
-        int i = 0;
-        while (i < s.length()) {
-            int j = i + 1;
-            while (j < s.length() && s.charAt(j) == s.charAt(i)) j++;
-            temp.add(j - i);
-            i = j;
+        int[] zero = new int[s.length()];
+        int[] one = new int[s.length()];
+        if (s.charAt(0) == '0') zero[0] = 1; else one[0] = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == '0') zero[i] = zero[i - 1] + 1; else one[i] = one[i - 1] + 1;
         }
-        int ans = 0;
-        for (i = 1; i < temp.size(); i++) {
-            ans += Math.min(temp.get(i), temp.get(i - 1));
+        int cnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                int idx = i - zero[i];
+                if (idx >= 0) {
+                    if (one[idx] >= zero[i]) cnt++;
+                }
+            } else {
+                int idx = i - one[i];
+                if (idx >= 0) {
+                    if (zero[idx] >= one[i]) cnt++;
+                }
+            }
         }
-        return ans;
+        return cnt;
     }
 }
