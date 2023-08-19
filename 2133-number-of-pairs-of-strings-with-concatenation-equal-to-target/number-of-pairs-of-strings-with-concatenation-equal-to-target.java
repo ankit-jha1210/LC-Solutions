346cdc44c6a112043405854ef86f638cd92672c5
Ankit Jha <jhaa11701@gmail.com>
2023-08-19 10:15:28 +0530

    Time: 3 ms (91.58%) | Memory: 41.5 MB (94.99%) - LeetSync

--- 2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/number-of-pairs-of-strings-with-concatenation-equal-to-target.java
+++ 2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/number-of-pairs-of-strings-with-concatenation-equal-to-target.java
@@ -1,13 +1,22 @@
 class Solution {
 
     public int numOfPairs(String[] nums, String target) {
+        HashMap<String, Integer> map = new HashMap<>();
         int cnt = 0;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (i != j && (nums[i] + nums[j]).equals(target)) {
-                    cnt++;
+        for (String num : nums) {
+            if (target.startsWith(num)) {
+                String sub = target.substring(num.length());
+                if (map.containsKey(sub)) {
+                    cnt += map.get(sub);
                 }
             }
+            if (target.endsWith(num)) {
+                String sub = target.substring(0, target.length() - num.length());
+                if (map.containsKey(sub)) {
+                    cnt += map.get(sub);
+                }
+            }
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
         return cnt;
     }
