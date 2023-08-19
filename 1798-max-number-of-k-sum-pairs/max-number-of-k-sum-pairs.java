f39a5b4e2d76dcd55fa40c263a68bb88b11e20bd
Ankit Jha <jhaa11701@gmail.com>
2023-08-19 09:25:02 +0530

    Time: 35 ms (36.81%) | Memory: 54.1 MB (95.53%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
@@ -2,17 +2,15 @@ class Solution {
 
     public int maxOperations(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        int ans = 0;
-        for(int num: nums) {
-            int req = k - num;
-            if(map.containsKey(req)) {
-                ans++;
-                int freq = map.get(req);
-                if(freq == 1) map.remove(req);
-                else map.put(req, freq - 1);
-            }
-            else map.put(num, map.getOrDefault(num, 0) + 1);
+        int cnt = 0;
+        for (int num : nums) {
+            if (map.containsKey(k - num)) {
+                int freq = map.get(k - num) - 1;
+                if (freq == 0) map.remove(k - num); 
+                else map.put(k - num, freq);
+                cnt++;
+            } else map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        return ans;
+        return cnt;
     }
 }
