95881f9ac7eca30ef967bc08ef70e3e3d771eb9c
Ankit Jha <jhaa11701@gmail.com>
2023-08-20 21:26:11 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (98.58%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -3,38 +3,10 @@ class Solution {
     public int[] sortedSquares(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
-         int idx = 0;
-        int neg = -1, pos = -1;
-        if(nums[n - 1] < 0) {
-            neg = n - 1;
-             while (neg >= 0) res[idx++] = nums[neg] * nums[neg--];
-             return res;
+        int i = 0, j = n - 1;
+        for (int p = n - 1; p >= 0; p--) {
+            if (Math.abs(nums[i]) > Math.abs(nums[j])) res[p] = nums[i] * nums[i++]; else res[p] = nums[j] * nums[j--];
         }
-        else if(nums[0] >= 0) {
-            pos = 0;
-            while (pos < n) res[idx++] = nums[pos] * nums[pos++];
-            return res;
-        }
-        for (int i = 1; i < nums.length; i++) {
-            if(nums[i - 1] < 0 && nums[i] >= 0) {
-                neg = i - 1;
-                pos = i;
-            }
-        }
-
-       
-
-        while (neg >= 0 && pos < n) {
-            if (-nums[neg] < nums[pos]) {
-                res[idx++] = nums[neg] * nums[neg--];
-            } else {
-                res[idx++] = nums[pos] * nums[pos++];
-            }
-        }
-
-        while (pos < n) res[idx++] = nums[pos] * nums[pos++];
-        while (neg >= 0) res[idx++] = nums[neg] * nums[neg--];
-
         return res;
     }
 }
