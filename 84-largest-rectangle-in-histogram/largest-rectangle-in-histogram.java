bc8a5786f60c000e9dd0643e00b09109fbe3cbf8
Ankit Jha <jhaa11701@gmail.com>
2023-08-25 10:24:21 +0530

    Time: 58 ms (76.12%) | Memory: 60 MB (6.71%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,27 +1,19 @@
 class Solution {
-
-    public int largestRectangleArea(int[] nums) {
-        int n = nums.length;
-        int[] nse = new int[n];
-        int[] pse = new int[n];
+    public int largestRectangleArea(int[] a) {
         Stack<Integer> stk = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            while (!stk.isEmpty() && nums[stk.peek()] >= nums[i]) stk.pop();
-            if (stk.isEmpty()) pse[i] = 0; else pse[i] = stk.peek() + 1;
+        int n = a.length;
+        int ans = 0;
+        // we are making nse and i pointer is finding pse of the top element in the stack
+        for(int i = 0;  i <= n; i++) {
+            while(!stk.isEmpty() && (i == n || a[stk.peek()] >= a[i])) {
+                int height = a[stk.pop()];
+                int width = 0;
+                if(stk.isEmpty()) width = i;
+                else width = i - stk.peek() - 1;
+                ans = Math.max(ans, width * height);
+            }
             stk.push(i);
         }
-        while(!stk.isEmpty()) stk.pop();
-        
-        for (int i = n - 1; i >= 0; i--) {
-            while (!stk.isEmpty() && nums[stk.peek()] >= nums[i]) stk.pop();
-            if (stk.isEmpty()) nse[i] = n - 1; else nse[i] = stk.peek() - 1;
-            stk.push(i);
-        }
-        int maxi = 0;
-        for (int i = 0; i < n; i++) {
-            int width = nse[i] - pse[i] + 1;
-            maxi = Math.max(maxi, width * nums[i]);
-        }
-        return maxi;
+        return ans;
     }
 }
