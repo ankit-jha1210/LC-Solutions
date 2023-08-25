55c0f974a05753876aeae07412a919d64eae8749
Ankit Jha <jhaa11701@gmail.com>
2023-08-25 12:01:17 +0530

    Time: 61 ms (73.17%) | Memory: 55 MB (91.33%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -5,7 +5,7 @@ class Solution {
         int ans = 0;
         // we are making nse and i pointer is finding pse of the top element in the stack
         for(int i = 0;  i <= n; i++) {
-            while(!stk.isEmpty() && (i == n || a[stk.peek()] >= a[i])) {
+            while(!stk.isEmpty() && (i == n || a[stk.peek()] > a[i])) {
                 int height = a[stk.pop()];
                 int width = 0;
                 if(stk.isEmpty()) width = i;
