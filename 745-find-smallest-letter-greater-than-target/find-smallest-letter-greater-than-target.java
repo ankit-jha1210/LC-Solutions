697a44101a9fc60193b971553d9228f90ad67321
Ankit Jha <jhaa11701@gmail.com>
2023-08-25 22:00:27 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (94.06%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -1,12 +1,11 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        if(letters[letters.length - 1] <= target) return letters[0];
         int low = 0, high = letters.length - 1;
         while(low <= high) {
             int mid = low + (high - low)/2;
             if(letters[mid] <= target) low = mid + 1;
             else high = mid - 1;  
         }
-        return letters[low];
+        return low == letters.length  ? letters[0] : letters[low];
     }
 }
